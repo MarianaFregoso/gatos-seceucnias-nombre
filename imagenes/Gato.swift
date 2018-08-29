51c50eca0057a22cca01ef745d7d2539ca854669
Maestro <Maestro@iMac-de-Maestro-3.local>
2018-08-28 18:46:22 -0700

    gatitos

--- imagenes/Gato.swift
+++ imagenes/Gato.swift
@@ -7,3 +7,10 @@
 //
 
 import Foundation
+
+class Gato {
+    var nombre : String?
+    var vidas : Int?
+    var edad : Int?
+    var esterelizado : Bool?
+}
--- imagenes/ViewController.swift
+++ imagenes/ViewController.swift
@@ -10,9 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbltexto: UILabel!
+    @IBOutlet weak var imgsecuencia: UIImageView!
+    @IBAction func doTapGato(_ sender: Any) {
+        
+        if imgsecuencia.isAnimating{
+            imgsecuencia.stopAnimating()
+            lbltexto.text = "¡Guau pariente!"
+        }else {
+            imgsecuencia.startAnimating()
+            lbltexto.text = "¡Continua!"
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imgsecuencia.image = UIImage(named: "Carta2_00000")
+        
+        var fotos : [UIImage] = []
+        fotos.append(UIImage(named: "Carta2_00000")!)
+        fotos.append(UIImage(named: "Carta2_00001")!)
+        fotos.append(UIImage(named: "Carta2_00002")!)
+        fotos.append(UIImage(named: "Carta2_00003")!)
+        fotos.append(UIImage(named: "Carta2_00004")!)
+        fotos.append(UIImage(named: "Carta2_00005")!)
+        fotos.append(UIImage(named: "Carta2_00006")!)
+        fotos.append(UIImage(named: "Carta2_00007")!)
+        
+        imgsecuencia.animationImages = fotos
+        imgsecuencia.animationDuration = 1
+        
+        imgsecuencia.startAnimating()
+        
+        let miGato = Gato()
+        miGato.nombre = "Don Perro"
+        miGato.vidas = 3
+        
+        // ! es unwrapping 
+        //primero validamos que miGato.nombre no sea nulo
+        if miGato.nombre != nil {
+            let nombregato : String = miGato.nombre!
+            lbltexto.text = nombregato
+        }
+       
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
